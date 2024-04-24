173f1a4144408198e1d0019d1af7879778b82858
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-24 09:16:23 +0100

    FILE: Addition of script 0, requirements.txt and readme file

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+
+args = sys.argv
+user_url = f'https://jsonplaceholder.typicode.com/users/{args[1]}'
+todo_url = 'https://jsonplaceholder.typicode.com/todos/'
+
+user_response = requests.get(user_url)
+todo_response = requests.get(todo_url)
+user_id = int(args[1])
+
+if user_response.status_code == 200:
+    # print(response)
+    data = user_response.json()
+    # print("Response data: ", data)
+else:
+    print("Error:", f"Status code: {user_response.status_code}")
+
+if todo_response.status_code == 200:
+    # print(todo_response)
+    todo_list = todo_response.json()
+    user_todo_list = [todo for todo in todo_list if todo['userId'] == user_id]
+    # print("Todo List: ", todo_list)
+    completed_tasks = []
+    no_of_tasks = len(user_todo_list)
+    for task in user_todo_list:
+        if task['completed'] == True:
+            completed_tasks.append(task)
+    # print("User's todo list: ", user_todo_list)
+    no_of_completed_tasks = len(completed_tasks)
+    print(f"Employee {data['name']} is done with tasks({no_of_completed_tasks}/{no_of_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task['title']}")
+else:
+    print("Error:", f"Status code: {todo_response.status_code}")
+
