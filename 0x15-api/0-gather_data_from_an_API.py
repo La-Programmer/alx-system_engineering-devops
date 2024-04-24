1cd2f789a58f9c147cbc09630da695d5dfe920a5
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-24 09:35:29 +0100

    FIX: Matching requirements

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,33 +10,38 @@ args = sys.argv
 user_url = f'https://jsonplaceholder.typicode.com/users/{args[1]}'
 todo_url = 'https://jsonplaceholder.typicode.com/todos/'
 
-user_response = requests.get(user_url)
-todo_response = requests.get(todo_url)
-user_id = int(args[1])
+def run():
+    """Main function"""
+    user_response = requests.get(user_url)
+    todo_response = requests.get(todo_url)
+    user_id = int(args[1])
 
-if user_response.status_code == 200:
-    # print(response)
-    data = user_response.json()
-    # print("Response data: ", data)
-else:
-    print("Error:", f"Status code: {user_response.status_code}")
+    if user_response.status_code == 200:
+        # print(response)
+        data = user_response.json()
+        # print("Response data: ", data)
+    else:
+        print("Error:", f"Status code: {user_response.status_code}")
 
-if todo_response.status_code == 200:
-    # print(todo_response)
-    todo_list = todo_response.json()
-    user_todo_list = [todo for todo in todo_list if todo['userId'] == user_id]
-    # print("Todo List: ", todo_list)
-    completed_tasks = []
-    no_tasks = len(user_todo_list)
-    for task in user_todo_list:
-        if task['completed'] is True:
-            completed_tasks.append(task)
-    # print("User's todo list: ", user_todo_list)
-    comp_tasks = len(completed_tasks)
-    print(
-        f"Employee {data['name']} is done with tasks({comp_tasks}/{no_tasks}):"
-        )
-    for task in completed_tasks:
-        print(f"\t {task['title']}")
-else:
-    print("Error:", f"Status code: {todo_response.status_code}")
+    if todo_response.status_code == 200:
+        # print(todo_response)
+        todo_list = todo_response.json()
+        user_todo_list = [todo for todo in todo_list if todo.get('userId') == user_id]
+        # print("Todo List: ", todo_list)
+        completed_tasks = []
+        no_tasks = len(user_todo_list)
+        for task in user_todo_list:
+            if task.get('completed') is True:
+                completed_tasks.append(task)
+        # print("User's todo list: ", user_todo_list)
+        comp_tasks = len(completed_tasks)
+        print(
+            f"Employee {data.get('name')} is done with tasks({comp_tasks}/{no_tasks}):"
+            )
+        for task in completed_tasks:
+            print(f"\t {task.get('title')}")
+    else:
+        print("Error:", f"Status code: {todo_response.status_code}")
+
+if __name__ == "__main__":
+    run()
