c51fd26a7e902acc0e82c603c6874e6c3b9bb614
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-05-08 12:24:04 +0100

    Add readme file and script 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module queries the reddit api to
+find all the subscribers of a subreddit
+"""
+
+import requests
+# https://www.reddit.com/
+
+
+def number_of_subscribers(subreddit):
+    user_agent = "justin"
+    headers = {"User-Agent": user_agent}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    subreddit_info = requests.get(url, headers=headers)
+    if subreddit_info.status_code == 200:
+        data = subreddit_info.json()
+        result_data = data["data"]
+        return (result_data["subscribers"])
+    return (0)
