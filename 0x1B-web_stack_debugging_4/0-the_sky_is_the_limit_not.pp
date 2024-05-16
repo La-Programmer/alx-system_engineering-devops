a264a6476ec86b89e65e3655190a54bd78eb0ae9
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-05-16 22:45:19 +0100

    Fix puppet manifest 0 again

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,15 @@
-# Manage the content of the file /etc/default/nginx
-file { 'nginx':
-  ensure  => file,
-  path    => '/etc/default/nginx',
-  content => 'ULIMIT="-n 500"'
+# Fix NginX web server to be able to handle many requests 
+exec { 'fix -- for nginx':
+  # modify the ULIMIT value
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  # Specify the correct path for the sed command
+  path    => '/bin/',
+  # Ensure that the command only runs when necessary
+  unless  => '/bin/grep -q "ulimit -n 4096" /etc/default/nginx',
 }
 
 service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => File['nginx'],
+  ensure  => running,
+  enable  => true,
+  require => Exec['fix -- for nginx'],  # Ensure Nginx is restarted only after ULIMIT is modified
 }
