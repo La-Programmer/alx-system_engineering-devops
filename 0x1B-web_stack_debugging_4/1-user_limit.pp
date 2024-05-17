ad71a93389b3b83a5f15971a76ea914f68e4c00a
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-05-17 11:56:45 +0100

    Fix puppet manifest 1 to be puppet-lint compliant

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,6 +2,6 @@
 exec {
   'fix os config':
   command => '/bin/sed -i "s/5/2048/g; s/4/1024/g" /etc/security/limits.conf',
-  path => '/bin/',
-  unless => '/bin/grep -q "holberton hard nofile 2048"',
+  path    => '/bin/',
+  unless  => '/bin/grep -q "holberton hard nofile 2048"',
 }
